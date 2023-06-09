b8e406400de0c97fdef058a6d2b6cf10d7c929f1
KirillShnyak <kshnyak@bk.ru>
2023-06-09 09:46:56 +0500

    DOM

--- src/7-dom.js
+++ src/7-dom.js
@@ -1,3 +1,36 @@
-// export function getApp(wrapper) {
-//   wrapper.innerHTML = <input />;
-// }
+export function getApp(wrapper) {
+  wrapper.innerHTML = `
+  <input />
+  <button hidden>Add</button>
+  <div class="list">
+    <p>1</p>
+    <p>2</p>
+    <p>3</p>
+  </div>
+
+  `;
+
+  const button = wrapper.querySelector("button");
+  const input = wrapper.querySelector("input");
+  const list = wrapper.querySelector(".list");
+
+  input.addEventListener("input", () => {
+    if (input.value) {
+      button.hidden = false;
+    } else {
+      button.hidden = true;
+    }
+  });
+
+  button.addEventListener("click", () => {
+    const p = document.createElement("p");
+    p.innerHTML = input.value;
+    list.prepend(p);
+    input.value = "";
+    button.hidden = true;
+    const allP = list.querySelectorAll("p");
+    if (allP.length > 5) {
+      allP[5].remove();
+    }
+  });
+}
--- src/7-dom.test.js
+++ src/7-dom.test.js
@@ -2,15 +2,79 @@ import { getApp } from "./7-dom";
 
 describe("getApp", () => {
   let el;
+  let input;
+  let button;
+
   beforeEach(() => {
     el = document.createElement("div");
+    getApp(el);
+    input = el.querySelector("input");
+    button = el.querySelector("button");
   });
+
+  function getPharagraphs() {
+    return [...el.querySelectorAll("p")].map((p) => p.innerHTML);
+  }
+
+  function isButtonHidden() {
+    return button.hidden;
+  }
+
+  function changeInputValue(value) {
+    input.value = value;
+    input.dispatchEvent(new Event("input"));
+  }
+
+  function clickButton() {
+    button.click();
+  }
+
+  function getInputValue() {
+    return input.value;
+  }
 
   it("getApp is a function", () => {
     expect(getApp).toBeInstanceOf(Function);
   });
   it("creates proper initial markup", () => {
-    getApp(el);
-    expect(el.querySelector("input")).not.toBe(null);
+    expect(input).not.toBe(null);
+    expect(button).toBeTruthy();
+    expect(getPharagraphs()).toEqual(["1", "2", "3"]);
+  });
+
+  it("show button with enterd text", () => {
+    changeInputValue(123);
+    expect(isButtonHidden()).toBe(false);
+
+    changeInputValue("");
+    expect(isButtonHidden()).toBe(true);
+  });
+
+  it("adds paragraph to the begining on pressing button with entered text", () => {
+    const value = `${Math.random()}`;
+    changeInputValue(value);
+    clickButton();
+    expect(getPharagraphs()).toEqual([value, "1", "2", "3"]);
+  });
+
+  it("clears input on adding paragraph and hides the button", () => {
+    const value = `${Math.random()}`;
+    changeInputValue(value);
+    clickButton();
+    expect(getInputValue()).toBe("");
+    expect(isButtonHidden()).toBe(true);
+  });
+
+  it("keeps max 5 pharagraphs", () => {
+    const value = `${Math.random()}`;
+    changeInputValue(value);
+    clickButton();
+    const value2 = `${Math.random()}`;
+    changeInputValue(value2);
+    clickButton();
+    const value3 = `${Math.random()}`;
+    changeInputValue(value3);
+    clickButton();
+    expect(getPharagraphs()).toEqual([value3, value2, value, "1", "2"]);
   });
 });
